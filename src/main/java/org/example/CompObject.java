74a42374f23a13eafb963e8794ed03778d3d2c43
Pavel Koudelny <koudelnypa@gmail.com>
2022-05-31 23:41:08 +0300

    Try to use Comparator interface

--- src/main/java/org/example/CompObject.java
+++ src/main/java/org/example/CompObject.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class CompObject implements Comparable<CompObject>{
     private static Generator<Integer> gen = CountingGenerator.makeRandomIntegerGenerator(100);
@@ -8,7 +9,7 @@ public class CompObject implements Comparable<CompObject>{
 
     @Override
     public int compareTo(CompObject rv) {
-        return (this.value==rv.value) ? 0 : ((this.value>rv.value)?1:-1);
+        return Integer.compare(this.value, rv.value);
     }
 
     @Override
@@ -18,13 +19,23 @@ public class CompObject implements Comparable<CompObject>{
                 '}';
     }
 
+    static class ReverseOrder implements Comparator<CompObject> {
+        @Override
+        public int compare(CompObject obj1, CompObject obj2) {
+            return Integer.compare(obj2.value, obj1.value);
+        }
+    }
+
     public static void main(String[] args) {
         CompObject[] arr = new CompObject[5];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = new CompObject();
         }
         System.out.println(Arrays.toString(arr));
-        Arrays.sort(arr);
+        Arrays.sort(arr, new ReverseOrder());
         System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.binarySearch(arr,arr[arr.length-1],new ReverseOrder()));
     }
+
+
 }
